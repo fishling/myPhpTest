7ffda3e4c4f2716269c3826d56ef81592ed021af
fish <18110900215@163.com>
2015-09-18 17:55:33 +0800

    PHP文件引用

--- funcTest.php
+++ funcTest.php
@@ -5,5 +5,12 @@
  * Date: 2015/9/17
  * Time: 17:31
  */
+    /*require 'funcs.php';
+    require_once 'funcs.php';*/
 
+    include 'funcs.php';
+    include 'funcs.php';
+    include_once 'funcs.php';
+
+    echo 'success';
 ?>
