8a0dc8baea7881f274fc3794978c7e79832251ad
fish <18110900215@163.com>
2015-11-05 16:59:54 +0800

    对象

---
+++ object/Cat.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fish
+ * Date: 2015/11/5
+ * Time: 16:29
+ */
+
+    class Cat {
+        public $name;
+        public $color;
+        public $age;
+    }
+
+    $cat1 = new Cat();
+    $cat1->name = '小白';
+    $cat1->color = '白色';
+    $cat1->age = 10;
+
+    $cat2 = new Cat();
+    $cat2->name = '小花';
+    $cat2->color = '花色';
+    $cat2->age = 100;
+
+?>
