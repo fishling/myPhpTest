0dfe7f4d6ac15330f8c5594540c9ac76fae1b82e
fish <18110900215@163.com>
2015-11-16 18:03:30 +0800

    类

---
+++ object/ObjectTest.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fish
+ * Date: 2015/11/16
+ * Time: 17:36
+ */
+
+class ObjectTest {
+    public $name;
+    public $color;
+    public $age;
+
+    function add($num1,$num2){
+        return $num1+$num2;
+    }
+}
--- object/objectTest.php
+++ object/objectTest.php
@@ -5,20 +5,20 @@
  * Date: 2015/11/12
  * Time: 13:19
  */
-    require_once 'Cat.class.php';
+    require_once 'ObjectTest.class.php';
 
-    $cat1 = new Cat();
-    $cat1->name = '小白';
-    $cat1->color = '白色';
-    $cat1->age = 10;
-    echo $cat1->name;
+    $object1 = new ObjectTest();
+    $object1->name = '小白';
+    $object1->color = '白色';
+    $object1->age = 10;
+    echo $object1->name;
 
-    $cat2 = new Cat; //括号可以不写
-    $cat2->name = '小花';
+    $object2 = new ObjectTest; //括号可以不写
+    $object2->name = '小花';
 
-    $cat3 = $cat1;
-    $cat3->age = 34;
-    echo $cat1->age; //34 这时cat1和cat3指向的是同一个地址
+    $object3 = $object1;
+    $object3->age = 34;
+    echo $object1->age; //34 这时object1和object3指向的是同一个地址
 
     function test($c,&$var,$arr){
         $c->age = 1;
@@ -27,12 +27,12 @@
     }
     $var = 'ab';
     $arr = array(0,10);
-    test($cat1,$var,$arr);
+    test($object1,$var,$arr);
     /*
      * 对象传入函数的时候，传入的是地址
      * 其他基本类型传入函数的时候，默认传入的是值；但是可以加&传入地址
      */
-    echo $cat1->age.'||'.$var.'||'.$arr[1];
-    echo '</br>'.$cat1->add(5,6);
+    echo $object1->age.'||'.$var.'||'.$arr[1];
+    echo '</br>'.$object1->add(5,6);
 
 ?>
