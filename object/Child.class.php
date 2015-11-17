1b146d6c588b5e42d1fa72f8912903e275d24425
fish <18110900215@163.com>
2015-11-17 17:39:50 +0800

    全局变量和静态变量，静态方法

--- object/Child.class.php
+++ object/Child.class.php
@@ -9,6 +9,7 @@
     class Child {
         public $name;
         public static $num=0;
+        public static $fee=0;
 
         public function __construct($name){
             $this->name = $name;
@@ -21,6 +22,15 @@
             //使用静态变量
             self::$num++;
             echo $this->name.'加入游戏</br>';
+        }
+
+        //静态方法，只能引用静态变量，不能引用成员变量
+        public static function enterSchool($fee){
+            self::$fee += $fee;
+        }
+
+        public static function getFee(){
+            return self::$fee;
         }
     }
 ?>
--- object/ChildPlay.php
+++ object/ChildPlay.php
@@ -12,12 +12,16 @@
 
     $child1 = new Child('小丽');
     $child1->joinGame();
+    $child1->enterSchool(500);
     $child2 = new Child('小美');
     $child2->joinGame();
+    Child::enterSchool(400);
     $child3 = new Child('小花');
     $child3->joinGame();
+    Child::enterSchool(450);
 
     echo '总共'.$global_num.'个小孩加入了游戏</br>';
     //Child::$num 和 $child3::$num 都可以访问这个静态变量
-    echo '总共'.Child::$num.'个小孩加入了游戏';
+    echo '总共'.Child::$num.'个小孩加入了游戏</br>';
+    echo '学费总共'.Child::getFee();
 ?>
