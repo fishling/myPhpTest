7e6a2103ab2d2d33a30fdc5192c106e46754f94b
fish <18110900215@163.com>
2015-11-17 09:50:24 +0800

    常量

--- basic/constTest.php
+++ basic/constTest.php
@@ -6,10 +6,12 @@
  * Time: 15:54
  */
     define("TAX_RATE",0.08);
-    echo "TAX_RATE=".TAX_RATE;
+    //echo "TAX_RATE=".TAX_RATE;
 
     //5.3以上版本
-    echo '</br>';
+    //echo '</br>';
     const TAX_RATE2 = 0.10;
-    echo "TAX_RATE2=".TAX_RATE2;
+    //echo "TAX_RATE2=".TAX_RATE2;
+
+    const Pai_Value = 3.14;
 ?>
