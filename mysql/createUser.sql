e25afb4bc18da38e1de353ab5a92f82f6af13934
fish <18110900215@163.com>
2015-12-01 10:05:34 +0800

    mysql数据库常用语句

---
+++ mysql/createUser.sql
@@ -0,0 +1,19 @@
+create table user1(
+  id int PRIMARY KEY auto_increment,
+  name varchar(32) NOT NULL,
+  password varchar(64) NOT NULL,
+  email varchar(128) NOT NULL,
+  age tinyint unsigned NOT NULL
+);
+
+insert into user(name,password,email,age) values ('李希',md5(123456),'lixi@163.com',29);
+
+--查看表结构
+show create table user;
+--查看数据库字符集
+show variables like 'char%';
+--转换user表的字符集
+ALTER TABLE `user` CONVERT TO CHARACTER SET UTF8;
+--设置数据库字符集(使用mysql command line client时设置)
+set character_set_client=gbk;
+set character_set_result=gbk;
